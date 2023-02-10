683e1388333b6608eb68ff8fca9f674e7dd8926b
Bryan Bernigen <bryanbernigen@gmail.com>
2023-02-10 12:48:51 +0700

    Open Camera

--- app/src/main/java/com/example/majika/CameraFragment.kt
+++ app/src/main/java/com/example/majika/CameraFragment.kt
@@ -1,13 +1,29 @@
 package com.example.majika
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +43,29 @@ class CameraFragment : Fragment() {
     private lateinit var toolbarMajika: Toolbar
     private lateinit var toolbarMajikaText: TextView
 
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+    private var myViewFinder: PreviewView? = null
+
+    private lateinit var safeContext: Context
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        safeContext = context
+    }
+
+    private fun getStatusBarHeight(): Int {
+        val resourceId = safeContext.resources.getIdentifier("status_bar_height", "dimen", "android")
+        return if (resourceId > 0) {
+            safeContext.resources.getDimensionPixelSize(resourceId)
+        } else 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -41,6 +80,7 @@ class CameraFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_camera, container, false)
+
     }
 
     companion object {
@@ -52,15 +92,21 @@ class CameraFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment CameraFragment.
          */
+        val TAG = "CameraXFragment"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        internal const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+        var isOffline = false // prevent app crash when goes offline
+
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CameraFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            CameraFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -72,5 +118,145 @@ class CameraFragment : Fragment() {
         toolbarMajikaText.setText(toolbarMajika.title)
         (activity as AppCompatActivity).setSupportActionBar(toolbarMajika)
         (activity as AppCompatActivity).getSupportActionBar()?.setDisplayShowTitleEnabled(false)
+
+        myViewFinder = view.findViewById(R.id.viewFinder)
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Setup the listener for take photo button
+        val cameraCaptureButton: Button = view.findViewById(R.id.take_picture_button)
+        cameraCaptureButton.setOnClickListener { takePhoto() }
+
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+//        cameraExecutor = Executors.newCachedThreadPool()
     }
+
+    private fun startCamera() {
+//        OpenCVLoader.initDebug()
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(safeContext)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            preview = Preview.Builder().build()
+
+            imageCapture = ImageCapture.Builder().build()
+
+//            imageAnalyzer = ImageAnalysis.Builder().build().apply {
+//                setAnalyzer(Executors.newSingleThreadExecutor(), CornerAnalyzer {
+//                    val bitmap = viewFinder.bitmap
+//                    val img = Mat()
+//                    Utils.bitmapToMat(bitmap, img)
+//                    bitmap?.recycle()
+//                    // Do image analysis here if you need bitmap
+//                })
+//            }
+            // Select back camera
+            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                //ASLI
+//                camera = cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalyzer, preview, imageCapture)
+                //ANALYSER DI KOMEN
+                camera = cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalyzer, preview, imageCapture)
+                preview?.setSurfaceProvider(myViewFinder?.createSurfaceProvider())
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(safeContext))
+
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create timestamped output file to hold the image
+        val photoFile = File(outputDirectory, SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Setup image capture listener which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(safeContext), object : ImageCapture.OnImageSavedCallback {
+            override fun onError(exc: ImageCaptureException) {
+                Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+            }
+
+            override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                val savedUri = Uri.fromFile(photoFile)
+                val msg = "Photo capture succeeded: $savedUri"
+                Toast.makeText(safeContext, msg, Toast.LENGTH_SHORT).show()
+                Log.d(TAG, msg)
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isOffline = true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isOffline = false
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(safeContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(safeContext, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
+//                finish()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    fun getOutputDirectory(): File {
+        val mediaDir = activity?.externalMediaDirs?.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else activity?.filesDir!!
+    }
+
+
+//    private class CornerAnalyzer(private val listener: CornersListener) : ImageAnalysis.Analyzer {
+//
+//        private fun ByteBuffer.toByteArray(): ByteArray {
+//            rewind()    // Rewind the buffer to zero
+//            val data = ByteArray(remaining())
+//            get(data)   // Copy the buffer into a byte array
+//            return data // Return the byte array
+//        }
+//
+//        @SuppressLint("UnsafeExperimentalUsageError")
+//        override fun analyze(imageProxy: ImageProxy) {
+//            if (!isOffline) {
+//                listener()
+//            }
+//            imageProxy.close() // important! if it is not closed it will only run once
+//        }
+//
+//    }
 }
